6438c99cb9127046b9da8af7ab5516020b93d7f5
Felipe Lopes <felipe.lopes@sasquati.com.br>
2015-07-28 18:48:59 -0400

    algumas correções

--- vhost.sh
+++ vhost.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # script
-#     vhost.sh
+#	vhost.sh
 #
 # what it does?
-#     Configure an Apache virtual host
+#	Configure an Apache virtual host
 #
-# usage
-#     sudo ./vhost.sh [<server_name> <user-owner>] | -v
-#  - <server_name>: domain name, without www, e.g. awesomeness.com
-#  - <user-owner>: user will be used, e.g. www-data
-#  - -v: show this message
+# usage: sudo ./vhost.sh <server_name> <user-owner>
+#	<server_name>: domain name, without www, e.g. awesomeness.com
+#	<user-owner>: user will be used, e.g. www-data
 #
-# author: felipe lopes - bolzin [at] gmail [dot] com
+# author
+#	felipe lopes - bolzin [at] gmail [dot] com
 #
 # Copyright (c) 2015 Felipe Lopes
 #
@@ -36,28 +35,26 @@
 SERVER_NAME=$1
 SERVER_ALIAS="www."$1
 SERVER_ADMIN="webmaster@$1"
-DOCUMENT_ROOT="$1/public_html"
+VHOST_DIR="/var/www/$1"
+DOCUMENT_ROOT="$VHOST_DIR/public_html"
 
+INDEX="<?php echo \"<h1>$1</h1>\"; ?>"
 VHOST_FILE="/etc/apache2/sites-available/$SERVER_NAME.conf"
-
-INDEX="<?php echo <h1>$1</h1> ?>"
-
-VHOST_CONTENT=$(cat <<EOF 
-<VirtualHost *:80>
-   ServerAdmin $SERVER_ADMIN
-   ServerName $SERVER_NAME
-   ServerAlias $SERVER_ALIAS
-   DocumentRoot $DOCUMENT_ROOT
-
-   ErrorLog ${APACHE_LOG_DIR}/error.log
-   CustomLog ${APACHE_LOG_DIR}/access.log combined
-</Virtualhost>
-EOF)
 
 # test if root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
+fi
+
+# test usage
+if [ $# -ne 2 ]; then
+cat <<EOF
+usage: sudo ./vhost.sh [<server_name> <user-owner>]
+	<server_name>: domain name, without www, e.g. awesomeness.com
+	<user-owner>: user will be used, e.g. www-data
+EOF
+	exit 1
 fi
 
 # test if apache2 exist
@@ -68,25 +65,39 @@ if [[ $? != 0 ]]; then
 fi
 
 # test if vhost is being used
-if [ ! -d $DOCUMENT_ROOT ]; then
-   echo "$DOCUMENT_ROOT already being used"
+if [ -d $VHOST_DIR ]; then
+   echo "$VHOST_DIR already being used"
    exit 1
 fi
 
 # change permission to /var/www to 755
-chmod 755 /var/www
+PERMISSION=$(stat -t --format=%a /var/www)
+if [ $PERMISSION != "755" ]; then chmod 755 /var/www; fi
 
 # create and set permission to document root
 mkdir -p $DOCUMENT_ROOT
-chown -R $2:$2 $DOCUMENT_ROOT
+chown -R "$2":"$2" $DOCUMENT_ROOT
 
 # create an index.php inside document root
 echo $INDEX > $DOCUMENT_ROOT/index.php
 
 # create vhost conf file
-echo $VHOST_CONTENT >> $VHOST_FILE
+cat <<EOF > "$VHOST_FILE"
+<VirtualHost *:80>
+	ServerAdmin $SERVER_ADMIN
+	ServerName $SERVER_NAME
+	ServerAlias $SERVER_ALIAS
+	DocumentRoot $DOCUMENT_ROOT
+
+	ErrorLog \${APACHE_LOG_DIR}/error.log
+	CustomLog \${APACHE_LOG_DIR}/access.log combined
+</Virtualhost>
+EOF
 
 # enable vhost
 a2ensite $SERVER_NAME
 
-service apache2 restart
+service apache2 reload
+
+exit 0
+
